5278b46a9cd315c4bbc5a6efaf6452452106433f
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-09-04 22:08:53 -0500

    reverse integer

---
+++ LeetCode/reverse-integer.py
@@ -0,0 +1,9 @@
+#Reverse digits of an integer
+class Solution:
+    # @return an integer
+    def reverse(self, x):
+        if x >= 0:
+            return int(str(x)[::-1])
+        else:
+            return int('-'+str(x)[-1:0:-1])
+        
