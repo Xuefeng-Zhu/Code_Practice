5d790ebcc7550455242e97c0f8d98366016cf5f1
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-09-04 23:12:53 -0500

    bTree preorder trav

---
+++ LeetCode/binary-tree-preorder-traversal.py
@@ -0,0 +1,18 @@
+# Given a binary tree, return the preorder traversal of its nodes' values.
+
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of integers
+    def preorderTraversal(self, root):
+        if root is None:
+            return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+        
